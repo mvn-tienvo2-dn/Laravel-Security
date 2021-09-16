72455c147bba38c30606dad09eedd2966339cf9a
tienvo <tien.vo2@monstar-lab.com>
2021-09-16 20:49:20 +0700

    update-midđleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,9 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(){
+        $this->middleware('auth');
+    }
     public function index()
     {
         $data['listPost'] = Post::all();
